415f4dcd74ba8ed66aedf8528179440f397e5a32
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-10-23 19:31:34 +0300

    init

--- HeaderFooterDemo/ContentView.swift
+++ HeaderFooterDemo/ContentView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    var europe = ["Finland", "Sweden", "Norway", "Denmark", "Estonia", "Latvia", "Lithuanina", "France", "Germany", "Italy", "Spaint", "Portugal"]
+    var asia = ["Pakistan", "India", "China", "Japan", "Russia", "Korea", "Afghanistan", "Iran", "Iraq", "Kazakhstan", "Kyrgyzstan", "Mongolia", "Nepal", "Tajikistan", "Turkmenistan", "Uzbekistan"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Section(header:
+                        Text("Europe")
+                            .foregroundColor(.primary)
+                ) {
+                    ForEach(0 ..< europe.count) {
+                        Text(self.europe[$0])
+                    }
+                }
+                
+                Section(header:
+                            HStack {
+                                Image(systemName: "airplane")
+                                Text("Asia")
+                                    .foregroundColor(.primary)
+                            }, footer:
+                                Text("Some footer text")
+                                .font(.footnote)
+                ) {
+                    ForEach(0 ..< asia.count) {
+                        Text(self.asia[$0])
+                    }
+                }
+            }.navigationBarTitle("Countries")
+        }
     }
 }
 
