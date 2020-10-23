a3482ee2888f7d39f17cd40bcc8b409c7dc1b4eb
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-10-23 19:08:16 +0300

    Initial Commit

---
+++ HeaderFooterDemo/HeaderFooterDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeaderFooterDemoApp.swift
+//  HeaderFooterDemo
+//
+//  Created by Ramill Ibragimov on 23.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct HeaderFooterDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
